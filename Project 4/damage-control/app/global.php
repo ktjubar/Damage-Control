90b822230b161dc21b620dec5c6cddb825813b29
Mihir Mallya <mmihir@vt.edu>
2018-03-27 13:57:24 -0400

    I made changes?

--- Project 4/damage-control/app/global.php
+++ Project 4/damage-control/app/global.php
@@ -5,3 +5,8 @@ session_start();
 set_include_path(dirname(__FILE__));
 
 include_once 'config.php';
+
+// this function automatically loads any classes in the specified folder
+function __autoload($className) {
+	require_once 'model/'.$className.'.class.php';
+}
