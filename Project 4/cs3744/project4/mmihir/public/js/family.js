e8c72f6bef839831d3d58ab933e7ec272b7d1ff2
Mihir M <mmihir@vt.edu>
2018-03-27 12:06:09 -0400

    Fixed file structure to not piss me off

--- Project 4/cs3744/project4/mmihir/public/js/family.js
+++
@@ -1,9 +0,0 @@
-function adjustGrid() {
-    "use strict";
-    var w = window.innerWidth;
-    var num = Math.floor(w/450);
-    document.getElementById("grid-container").style.gridTemplateColumns = "8cm ".repeat(num);
-}
-
-window.onload = adjustGrid;
-window.onresize = adjustGrid;
