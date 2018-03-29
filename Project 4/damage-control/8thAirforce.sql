43431264ad52ce191c77534604805474832efc2f
Mary Haninger <maryh15@vt.edu>
2018-03-29 16:47:21 -0400

    Example data in sql

--- Project 4/damage-control/8thAirforce.sql
+++ Project 4/damage-control/8thAirforce.sql
@@ -34,6 +34,13 @@ CREATE TABLE `Crews` (
   `Creator_ID` int(11) UNSIGNED NOT NULL,
   `Date_Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Dumping data for table `Crews`
+--
+
+INSERT INTO `Crews` (`ID`, `Name`, `Creator_ID`, `Date_Created`) VALUES
+(91, '91st Bomb Group: The Ragged Irregulars', 1, '2018-03-29 19:03:29'),
+(381, '381st Bomb Group', 1, '2018-03-29 19:04:19');
 
 -- --------------------------------------------------------
 
@@ -52,6 +59,18 @@ CREATE TABLE `Soldiers` (
   `Creator_ID` int(11) UNSIGNED NOT NULL,
   `Date_Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `Soldiers`
+--
+
+INSERT INTO `Soldiers` (`ID`, `First_Name`, `Last_Name`, `Rank`, `Date_of_Birth`, `Date_of_Death`, `Crew_ID`, `Creator_ID`, `Date_Created`) VALUES
+(1, 'Stanley', 'Wray', 6, '1907-09-15', '1986-09-13', 91, 1, '2018-03-29 20:13:25'),
+(2, 'Baskin', 'Lawrence', 9, '1905-03-25', NULL, 91, 1, '2018-03-29 20:29:50'),
+(3, 'Claude', 'Putnam', 6, '1915-10-07', '1965-05-06', 91, 1, '2018-03-29 20:33:55'),
+(4, 'Harry', 'Leber', 6, '1911-07-14', '1966-02-16', 381, 1, '2018-03-29 20:36:41'),
+(5, 'Melvin', 'Hecker', 4, NULL, '1975-03-01', 381, 1, '2018-03-29 20:39:58'),
+(6, 'Conway', 'Hall', 6, NULL, NULL, 381, 1, '2018-03-29 20:44:59');
 
 --
 -- Indexes for dumped tables
