492b44ad49acf44a82a6f2644e8c70671c48164d
Kaleb Jubar <ktjubar@vt.edu>
2018-03-30 00:05:56 -0400

    fixed BASE_URL

--- Project 4/working/app/config.php
+++ Project 4/working/app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL', 'http://localhost'); # your base URL
+define('BASE_URL', 'http://ec2-34-235-121-226.compute-1.amazonaws.com/'); # your base URL
 
 define('DB_USER', 'root');
 define('DB_PASS', '');
