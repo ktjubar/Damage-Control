d7ab29e31418adab8d3743c9f23b69ab23263c72
EC2 Default User <ec2-user@ip-172-31-46-215.ec2.internal>
2018-05-05 18:45:48 +0000

    Fixed config.php again

--- Project 4/working/cs3744/project5/DamageControl/app/config.php
+++ Project 4/working/cs3744/project5/DamageControl/app/config.php
@@ -2,8 +2,8 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-// define('BASE_URL', 'http://ec2-34-235-121-226.compute-1.amazonaws.com'); # your base URL
-define('BASE_URL', 'http://localhost/DamageControl');
+define('BASE_URL', 'http://ec2-34-235-121-226.compute-1.amazonaws.com'); # your base URL
+// define('BASE_URL', 'http://localhost/DamageControl');
 
 define('DB_USER', 'root');
 define('DB_PASS', 'jubar123456');
